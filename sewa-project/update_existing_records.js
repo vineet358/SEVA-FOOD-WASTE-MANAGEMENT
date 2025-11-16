24c4c567515862f3118364752e3b15089da83da2
Anjali483-prog <annumehra175@gmail.com>
2025-11-16 20:18:28 +0530

    CSS changes

--- sewa-project/update_existing_records.js
+++ sewa-project/update_existing_records.js
@@ -1,10 +1,14 @@
-
+// Update city for a specific hotel
 db.hotels.updateOne(
   { _id: ObjectId("68f7e28aadb8e73df9a92bab") },
-  { $set: { city: "haldwani" } }
+  { 
+    $set: { 
+      city: "haldwani" 
+    } 
+  }
 );
 
-
+// Update city and coordinates for a specific food entry
 db.foods.updateOne(
   { _id: ObjectId("68f7e39badb8e73df9a92bb0") },
   { 
@@ -16,10 +20,9 @@ db.foods.updateOne(
   }
 );
 
-
+// Logging updated documents for verification
 console.log("Hotel after update:");
-db.hotels.findOne({ _id: ObjectId("68f7e28aadb8e73df9a92bab") });
+printjson(db.hotels.findOne({ _id: ObjectId("68f7e28aadb8e73df9a92bab") }));
 
 console.log("Food after update:");
-db.foods.findOne({ _id: ObjectId("68f7e39badb8e73df9a92bb0") });
-
+printjson(db.foods.findOne({ _id: ObjectId("68f7e39badb8e73df9a92bb0") }));
